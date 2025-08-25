5e465fe48324e69e21219b63b61516946f3e4aed
Usuario755 <425114755@pcpuma.acatlan.unam.mx>
2025-08-25 08:09:34 -0600

    Update Se intento1.c

--- Se intento1.c
+++ Se intento1.c
@@ -26,7 +26,7 @@ void facTail(n,a){
 int main (int argc, char *argv[]){
     int i,n,a;
     char *num =argv[1];
-    printf(stdout, "arg[1] = %s \n", argv[1]);
+    fprintf(stdout, "arg[1] = %s \n", argv[1]);
     
     for(i=0;i<strlen(argv[1]);i++)
         fprintf(stdout,"num[%d]=%c \n", i, num[i]);
